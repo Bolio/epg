81983d94814d1435b2a0004f7e5f6ab0d7876435
Manuel Bolio <again.bolio@hotmail.com>
2024-03-12 07:03:50 -0600

    Add 'tvShowInfoSlice' slice.

---
+++ src/Redux/slices/Epg/tvShowInfoSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface ActivitiesState {
+  title: string;
+  airtime: string;
+  description: string;
+}
+
+const initialState: ActivitiesState = {
+  title: "",
+  airtime: "",
+  description: "",
+};
+
+const tvShowInfoSlice = createSlice({
+  name: "TV Show Info",
+  initialState,
+  reducers: {
+    title: (state, action) => {
+      state.title = action.payload;
+    },
+    airtime: (state, action) => {
+      state.airtime = action.payload;
+    },
+    description: (state, action) => {
+      state.description = action.payload;
+    },
+  },
+});
+
+export const { title, airtime, description } = tvShowInfoSlice.actions;
+
+export default tvShowInfoSlice.reducer;
