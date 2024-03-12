7078137f1e47e7be76850bf2f5393bbe8dc8b07f
Manuel Bolio <again.bolio@hotmail.com>
2024-03-12 06:37:05 -0600

    Add 'HomePage' component.

---
+++ src/Pages/Home/HomePage.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button } from "../../Components/Button";
+import { Modal } from "../../Components/Modal";
+import styled from "styled-components";
+
+const StyledHome = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <StyledHome>
+      <Button
+        type="button"
+        backgroundColor="#007bff"
+        handleClick={handleToggleModal}
+      >
+        Mostrar EPG
+      </Button>
+      <Modal handleToggleModal={handleToggleModal} showModal={showModal} />
+    </StyledHome>
+  );
+};
+
+export default HomePage;
